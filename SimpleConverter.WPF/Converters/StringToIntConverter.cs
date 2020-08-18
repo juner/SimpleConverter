ba2894929ebc5003efb9a727f2668edc026907f6
juner <juner@juner.net>
2020-08-19 08:56:19 +0900

    first commit.

---
+++ SimpleConverter.WPF/Converters/StringToIntConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace GenericConverter
+{
+    public class StringToIntConverter : SimpleValueConverter<string, NumberStyles, int?>
+    {
+        public override int? Convert(string value, NumberStyles parameter, CultureInfo culture)
+            => int.TryParse(value, parameter, culture, out var result) ? result : (int?)null;
+
+        public override string ConvertBack(int? value, NumberStyles parameter, CultureInfo culture)
+            => value?.ToString(parameterToFormatString(parameter), culture) ?? string.Empty;
+        private string parameterToFormatString(NumberStyles parameter)
+            => (parameter | NumberStyles.AllowCurrencySymbol) == NumberStyles.AllowCurrencySymbol ? "C"
+            : (parameter | NumberStyles.AllowHexSpecifier) == NumberStyles.AllowHexSpecifier ? "X"
+            : "D";
+    }
+}
